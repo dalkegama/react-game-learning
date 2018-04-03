6b52c24831bf34f2872a4cdbedf5b01d2eb26d8f
dalkegama <dhanik@greatsparrow.com>
2018-04-03 18:50:13 +0100

    fixed messed up code

--- src/index.js
+++ src/index.js
@@ -4,61 +4,70 @@ import "./index.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const Stars = (props) => {
-  const range = Array.from({
-    length: props.numberOfStars
-  }, (v, k) =>
-      k
+const Stars = props => {
+  const range = Array.from(
+    {
+      length: props.numberOfStars
+    },
+    (v, k) => k
   );
 
   return (
     <div className="col-5">
-      {range.map(i =>
-        <i key={i} className="fa fa-star"></i>
-      )}
+      {range.map(i => <i key={i} className="fa fa-star" />)}
     </div>
-  )
-}
+  );
+};
 
-const Button = (props) => {
+const Button = props => {
   return (
     <div className="col-2">
-      <button className="btn btn-primary" disabled={props.selectedNumbers.length === 0}>=</button>
+      <button
+        className="btn btn-primary"
+        disabled={props.selectedNumbers.length === 0}> = </button>
     </div>
-  )
-}
+  );
+};
 
-const Answer = (props) => {
+const Answer = props => {
   return (
     <div className="col-5">
-      {props.selectedNumbers.map((number, i) =>
-        <span key={i} onClick={() => [props.unselectNumber(number)]}>{number}</span>
-      )}
+      {props.selectedNumbers.map((number, i) => (
+        <span key={i} onClick={() => [props.unselectNumber(number)]}>
+          {number}
+        </span>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-const Numbers = (props) => {
-  const numberClassName = (number) => {
+const Numbers = props => {
+  const numberClassName = number => {
     if (props.selectedNumbers.indexOf(number) >= 0) {
-      return 'selected';
+      return "selected";
     }
-  }
+  };
   return (
     <div className="card text-center">
       <div>
-        {Numbers.list.map((number, i) =>
-          <span key={i} className={numberClassName(number)} onClick={() => props.selectNumber(number)}>{number}</span>
-        )}
+        {Numbers.list.map((number, i) => (
+          <span
+            key={i}
+            className={numberClassName(number)}
+            onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 //This create a JavaScript array containing 1…N
-Numbers.list = Array.from({
-  length: 9
-}, (v, k) =>
-    k + 1
+Numbers.list = Array.from(
+  {
+    length: 9
+  },
+  (v, k) => k + 1
 );
 
 class Game extends React.Component {
@@ -66,17 +75,21 @@ class Game extends React.Component {
     selectedNumbers: [],
     randomNumOfStars: 1 + Math.floor(Math.random() * 9)
   };
-  selectNumber = (clickedNumber) => {
-    if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
+  selectNumber = clickedNumber => {
+    if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) {
+      return;
+    }
     this.setState(prevState => ({
       selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
-    }))
-  };
-  unselectNumber = (clickedNumber) => {
-    this.setState(prevState => ({
-      selectedNumbers: prevState.selectedNumbers.filter(number => number !== clickedNumber)
     }));
-  }
+  };
+  unselectNumber = clickedNumber => {
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.filter(
+        number => number !== clickedNumber
+      )
+    }));
+  };
   render() {
     const { randomNumOfStars, selectedNumbers } = this.state;
     return (
@@ -87,14 +100,16 @@ class Game extends React.Component {
           <Button selectedNumbers={selectedNumbers} />
           <Answer
             selectedNumbers={selectedNumbers}
-            unselectNumber={this.unselectNumber} />
+            unselectNumber={this.unselectNumber}
+          />
         </div>
         <br />
         <Numbers
           selectedNumbers={selectedNumbers}
-          selectNumber={this.selectNumber} />
+          selectNumber={this.selectNumber}
+        />
       </div>
-    )
+    );
   }
 }
 
@@ -104,8 +119,8 @@ class App extends React.Component {
       <div>
         <Game />
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(< App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
